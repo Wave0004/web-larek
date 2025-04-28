573c011b2a68e988040995e999ffa6101f289dc8
Wave0004 <wave.capuletti@gmail.com>
2025-04-28 11:53:11 +0300

    added instances of shopping cart, customer, order forms, modal window forms + product test cards

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import './scss/styles.scss';
-
 import { cloneTemplate } from './utils/utils';
 import { EventEmitter } from './components/base/events';
 import { ItemView } from './components/ItemView';
@@ -7,9 +6,98 @@ import { ModalView } from './components/ModalView';
 import { CartView } from './components/CartView';
 import { CartModel } from './components/CartModel';
 import { CustomerModel } from './components/CustomerModel';
-import { PaymentType } from './types';
+import { PaymentType, ModalOpenEventData, IItem } from './types';
 import { OrderFormView } from './components/OrderFormView';
 import { ContactsFormView } from './components/ContactsFormView';
 
+const templateGalleryCard = document.querySelector('#card-catalog') as HTMLTemplateElement;
+
 
 const events = new EventEmitter();
+
+const cartData = new CartModel(events);
+
+const cartView = new CartView(events);
+
+const customerData = new CustomerModel();
+
+const modalView = ModalView.getInstance(events);
+
+const orderForm = new OrderFormView(events);
+
+const contactsForm = new ContactsFormView(events);
+
+
+const testItems = [
+  {
+  "id": "1",
+  "description": "Если планируете решать задачи в тренажёре, берите два.",
+  "image": "./images/bg1s.jpg",
+  "title": "название1",
+  "category": "софт-скил",
+  "price": 350
+  },
+
+  {
+    "id": "2",
+    "description": "Если планируете решать задачи в тренажёре, берите два.",
+    "image": "./images/bg1s.jpg",
+    "title": "название2",
+    "category": "софт-скил",
+    "price": 50
+    },
+
+  {
+    "id": "3",
+    "description": "Если планируете решать задачи в тренажёре, берите два.",
+    "image": "./images/bg1s.jpg",
+    "title": "название3",
+    "category": "софт-скил",
+    "price": 100
+    },
+
+  {
+    "id": "4",
+    "description": "Если планируете решать задачи в тренажёре, берите два.",
+    "image": "./images/bg1s.jpg",
+    "title": "название4",
+    "category": "софт-скил",
+    "price": 7500
+    }
+];
+
+const gallery = document.querySelector('.gallery') as HTMLElement;
+const cards = testItems.map((item) => {
+  const card = new ItemView(cloneTemplate(templateGalleryCard), item, events);
+  gallery.prepend(card.render());
+  return card;
+})
+
+
+events.on<ModalOpenEventData>('modal:open', (item) => {
+  modalView.openModal(item.element as HTMLElement);
+});
+
+events.on<ModalOpenEventData>('cart:remove', (item) => {
+  cartData.remove(item.data);
+});
+events.on<ModalOpenEventData>('cart:add', (item) => {
+  console.log(item.data.price);
+  cartData.add(item.data);
+})
+
+events.on<{items: string[]}>('cart:change', (data) => {
+  cartView.clear();
+  cards.forEach(item => {
+    data.items.forEach(dataItem => {
+      if(item.data.id === dataItem) {
+        cartView.addItem(item.cartItemView, item.data.id, cartData.sum);
+      }
+    })
+  })
+})
+events.on<[id: string]>('cart:submit', (data) => {
+  cartData.orderedItems = data;
+  modalView.openModal(orderForm.render());
+})
+
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,111 +1,114 @@
 export enum PaymentType {
-    Online = 'online',
-    Cash = 'cash',
-  }
-  
-  export interface IItem {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number | null;
-  }
-  
-  export interface ICustomerModel {
-    customerFullInfo: ICustomer;
-    validateCustomerAddress(data: string): boolean;
-    validateCustomerEmail(data: string): boolean;
-    validateCustomerPhoneNumber(data: string): boolean;
-  }
-  
-  export interface ICustomer {
-    paymentType: PaymentType;
-    address: string;
-    email: string;
-    phoneNumber: string;
-  }
-  
+  Online = 'online',
+  Cash = 'cash',
+}
 
-  export interface IItemsData {
-    items: IItem[];
-    setItems(items: IItem[]): void;
-    getItem(id: string): IItem;
-    preview: string | null; 
-  }
-  
-  export type TItemBaseInfo = Pick<IItem, 'category' | 'title' | 'image' | 'price' | 'id'>;
-  
-  export type TItemShortInfo = Pick<IItem, 'title' | 'price' | 'id'>;
-  
-  export type TItemFullInfo = Pick<IItem, 'image' | 'category' | 'title' | 'description' | 'price' | 'id'>;
-  
 
-  export interface ICartModel {
-    items: Map<string, number>;
-    add(id: string): void;
-    remove(id: string): void;
-  }
-  
-  export interface IEventEmitter {
-    emit: (event: string, data: unknown) => void
-  }
-  
-  export interface ICartView {
-    items: HTMLElement[];
-    parentContainer: HTMLElement;
-    container: HTMLElement;
-    submitButton: HTMLButtonElement;
-    cartButton: HTMLButtonElement;
-    price: HTMLSpanElement;
-  
-    render(): HTMLElement
-  }
+export interface IItem {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null; 
+}
 
-  export interface IContactsFormView {
-    form: HTMLFormElement;
-    submitButton: HTMLButtonElement;
-    emailInput: HTMLInputElement;
-    phoneInput: HTMLInputElement;
-  
-    render(): void;
-    toggleSubmitButton(): void;
-  }
 
-  export interface IOrderFormView {
-    form: HTMLFormElement;
-    paymentCashButton: HTMLButtonElement;
-    paymentCardButton: HTMLButtonElement;
-    submitButton: HTMLButtonElement;
-    adressInput: HTMLInputElement;
-  
-    render(): HTMLFormElement;
-  }
-  
-  export interface IItemView {
-    render(data: Partial<IItem>): HTMLElement;
-    category: HTMLSpanElement | null;
-    title: HTMLHeadingElement | HTMLSpanElement | null;
-    image: HTMLImageElement | null;
-    price: HTMLSpanElement | null;
-    description: HTMLParagraphElement | null;
-  
-    cartButton: HTMLButtonElement | null;
-  }
-  
-  export interface IModalView {
-    closeButton: HTMLElement;
-    parentContainer: HTMLElement;
-    container: HTMLElement;
-    events: IEventEmitter;
-  
-    openModal: (element: HTMLElement) => void;
-    closeModal: () => void;
-  }
-  
-  export interface IView {
-    element: HTMLElement,
-    container: HTMLElement,
-    render(data?: unknown): HTMLElement
-  }
-  
+export interface ICustomerModel {
+  customerFullInfo: ICustomer;
+  validateCustomerAddress(data: string): boolean;
+  validateCustomerEmail(data: string): boolean;
+  validateCustomerPhoneNumber(data: string): boolean;
+}
+
+
+export interface ICustomer {
+  paymentType: PaymentType;
+  address: string;
+  email: string;
+  phoneNumber: string;
+}
+
+
+export interface IItemsData {
+  items: IItem[];
+  setItems(items: IItem[]): void;
+  getItem(id: string): IItem;
+  preview: string | null; 
+}
+
+export type TItemBaseInfo = Pick<IItem, 'category' | 'title' | 'image' | 'price' | 'id'>;
+
+export type TItemShortInfo = Pick<IItem, 'title' | 'price' | 'id'>;
+
+export type TItemFullInfo = Pick<IItem, 'image' | 'category' | 'title' | 'description' | 'price' | 'id'>;
+
+export interface ICartModel {
+  items: Map<string, number>;
+  add(item: Partial<IItem>): void;
+  remove(item: Partial<IItem>): void;
+}
+
+export interface IEventEmitter {
+  emit: (event: string, data: unknown) => void
+}
+
+export interface ICartView {
+  render(): HTMLElement
+}
+
+export interface IContactsFormView {
+  render(): void;
+  toggleSubmitButton(): void;
+}
+
+export interface IOrderFormView {
+  form: HTMLFormElement;
+  paymentCashButton: HTMLButtonElement;
+  paymentCardButton: HTMLButtonElement;
+  submitButton: HTMLButtonElement;
+  adressInput: HTMLInputElement;
+
+  render(): HTMLFormElement;
+}
+
+export interface IItemView {
+  render(): HTMLElement;
+  category: HTMLSpanElement | null;
+  title: HTMLHeadingElement | HTMLSpanElement | null;
+  image: HTMLImageElement | null;
+  price: HTMLSpanElement | null;
+  description: HTMLParagraphElement | null;
+
+  addCartButton: HTMLButtonElement | null;
+  removeCartButton: HTMLButtonElement | null;
+}
+
+
+export interface IModalView {
+  openModal: (element: HTMLElement) => void;
+  closeModal: () => void;
+}
+
+export interface IView {
+  element: HTMLElement,
+  container: HTMLElement,
+  render(data?: unknown): HTMLElement
+}
+
+
+export interface ModalOpenEventData {
+  element: HTMLElement;
+  data?: Partial<IItem>;
+}
+
+
+export type TOrder = [
+  payment: PaymentType,
+  email: string,
+  phone: string,
+  address: string,
+  total: number,
+  items: string[],
+]
+
